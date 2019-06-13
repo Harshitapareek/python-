b373e1c3c780d52034d6dd15490a36595115e0ce
Harshitapareek <harshitapareek987@gmail.com>
2019-06-13 09:37:30 +0530

    Create problem1.py

---
+++ problem1.py
@@ -0,0 +1,14 @@
+problem1
+
+
+
+adhoc=[1,2,3,1,4,5,66,22,2,6,0,9]
+list=[]
+list1=[]
+for i in adhoc:
+   if(i>5):
+    list.append(i)
+   elif(i>=2):
+    list1.append(i)
+print(list)
+print(list1)
